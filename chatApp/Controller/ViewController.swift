19177c8b4bad05df041b7cb87185139b9f09ecec
志賀大河 <shigataiga@shigataiganoMacBook-Pro.local>
2020-01-14 00:31:07 +0900

    新規登録画面とログイン画面の作成

---
+++ chatApp/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  chatApp
+//
+//  Created by 志賀大河 on 2020/01/13.
+//  Copyright © 2020 志賀大河. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
