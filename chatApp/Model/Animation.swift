73b4ecdd6ac5d1fc76a75456298ad3eaf9058d28
志賀大河 <shigataiga@shigataiganoMacBook-Pro.local>
2020-01-17 19:41:07 +0900

    navigationボタンの文言変更

--- chatApp/Model/Animation.swift
+++ chatApp/Model/Animation.swift
@@ -7,7 +7,35 @@
 //
 
 import Foundation
+import Lottie
 
 class Animation {
    
+    // アニメーションのメソッド作り
+       // start
+       func startAnimation() {
+           
+           let animation = Animation.named("loading")
+           // loadingのデザイン
+           animationView.frame = CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height/1.5)
+           
+           animationView.animation = animation
+           animationView.contentMode = .scaleAspectFit
+           // アニメーションのループ
+           animationView.loopMode = .loop
+           // loadingの再生
+           animationView.play()
+           
+           view.addSubview(animationView)
+           
+       }
+       
+       //　アニメーション
+       // stop
+       func stopAnimation() {
+           // アニメーションを排除する
+           animationView.removeFromSuperview()
+       }
+       
+    
 }
